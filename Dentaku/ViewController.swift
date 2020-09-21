143ec682ed59b8dd10d31f81531e17c718c5a64b
shifei.chen <chenshifei.j@gmail.com>
2020-09-21 14:40:54 +0200

    Setting up dev env

---
+++ Dentaku/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dentaku
+//
+//  Created by Shifei Chen on 2020-09-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
